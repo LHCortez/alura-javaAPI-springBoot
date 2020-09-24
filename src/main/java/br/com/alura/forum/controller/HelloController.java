b9a914ca27ee55e61ab374b24fac2674a0da9d66
LHCortez <luizcquintas@gmail.com>
2020-09-24 15:09:00 -0300

    Endpoints

---
+++ src/main/java/br/com/alura/forum/controller/HelloController.java
@@ -0,0 +1,16 @@
+package br.com.alura.forum.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloController {
+
+	@RequestMapping("/")
+	@ResponseBody  //anotação @ResponseBody indica que o retorno do método deve ser serializado e devolvido no corpo da resposta.
+	public String hello() {
+		return "Hello World!";
+	}
+
+}
