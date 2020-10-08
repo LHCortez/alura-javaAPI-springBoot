52ec56a28e82ede0b0b871f5704271000d6bf1ae
LHCortez <luizcquintas@gmail.com>
2020-10-08 18:16:00 -0300

    Autorização baseada em Roles

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,4 +1,11 @@
 INSERT INTO USUARIO(nome, email, senha) VALUES('Aluno', 'aluno@email.com','$2a$10$fYnOXv7ENemKke3Ui3jivuk01DJ66VP9TUr2JZwcBXswwgkFxnyXm');
+INSERT INTO USUARIO(nome, email, senha) VALUES('Moderador', 'moderador@email.com','$2a$10$fYnOXv7ENemKke3Ui3jivuk01DJ66VP9TUr2JZwcBXswwgkFxnyXm');
+
+INSERT INTO PERFIL (id, nome) VALUES (1, 'ROLE_ALUNO');
+INSERT INTO PERFIL (id, nome) VALUES (2, 'ROLE_MODERADOR');
+
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES (1,1);
+INSERT INTO USUARIO_PERFIS(usuario_id, perfis_id) VALUES (2,2);
 
 INSERT INTO CURSO(nome, categoria) VALUES('Spring Boot', 'Programação');
 INSERT INTO CURSO(nome, categoria) VALUES('HTML 5', 'Front-end');
