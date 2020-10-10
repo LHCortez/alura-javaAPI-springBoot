a14be3ba544d0ef1d513e20c0c40deea06a14665
LHCortez <luizcquintas@gmail.com>
2020-10-09 21:38:32 -0300

    Testando um repository

--- src/test/java/br/com/alura/forum/ForumApplicationTests.java
+++ src/test/java/br/com/alura/forum/ForumApplicationTests.java
@@ -1,13 +1,19 @@
 package br.com.alura.forum;
 
+import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith (SpringRunner.class)
 @SpringBootTest
 class ForumApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		Assert.assertTrue(true);
 	}
 
 }
